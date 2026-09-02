89f1cf1b52075cd73cbdf2911d69743084ecc11d
khonchaiherb-dev <khonchaiherb@gmail.com>
2026-09-03 04:24:53 +0700

    chore: cache structural CSS at v1.34.5

--- public/sw.js
+++ public/sw.js
@@ -30,7 +30,7 @@ const CORE=[
   '/tshop-v116.css?v=1.18.2',
   '/tshop-v117.css?v=1.18.2',
   '/tshop-v118.css?v=1.18.2',
-  '/tshop-v134-structural-storefront.css?v=1.34.4',
+  '/tshop-v134-structural-storefront.css?v=1.34.5',
   '/tshop-v1345-header-isolation.css?v=1.34.5',
   '/app-core.js','/app-shop.js','/app-account.js','/app-admin-actions.js',
   '/tshop-v04.js','/tshop-v05.js','/tshop-v06.js','/tshop-v07.js','/tshop-v08.js','/tshop-v09.js','/tshop-v10.js','/tshop-v11.js','/tshop-v12.js','/tshop-v121.js','/tshop-v13.js','/tshop-v14.js','/tshop-v15.js','/tshop-v16.js',
