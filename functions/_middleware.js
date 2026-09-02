c17e15353a08a969869f29db57e55114c41f6473
khonchaiherb-dev <khonchaiherb@gmail.com>
2026-09-02 15:11:13 +0700

    chore: load commerce integrity v1.28.1

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -25,7 +25,7 @@ const withCanonicalMetadata=(response,env)=>{
     head.append('<link rel="stylesheet" href="/tshop-v128-future-standard.css?v=1.28.0">',{html:true});
     head.append('<script defer src="/kch-thai-first.js?v=1.23.1"></script>',{html:true});
     head.append('<script defer src="/kch-footer-premium.js?v=1.27.0"></script>',{html:true});
-    head.append('<script defer src="/kch-future-standard.js?v=1.28.0"></script>',{html:true});
+    head.append('<script defer src="/kch-future-standard.js?v=1.28.1"></script>',{html:true});
   }}).transform(response);
 };
 export async function onRequest({request,env,next}){
