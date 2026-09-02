310e204741ab6f85ea7e2641b918afc7d0b75ea6
khonchaiherb-dev <khonchaiherb@gmail.com>
2026-09-03 00:53:19 +0700

    feat: load v1.32 conversion storefront after v1.31

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -26,11 +26,13 @@ const withCanonicalMetadata=(response,env)=>{
     head.append('<link rel="stylesheet" href="/tshop-v129-readable-future.css?v=1.29.0">',{html:true});
     head.append('<link rel="stylesheet" href="/tshop-v130-koonchaishop.css?v=1.30.0">',{html:true});
     head.append('<link rel="stylesheet" href="/tshop-v131-top-conversion.css?v=1.31.0">',{html:true});
+    head.append('<link rel="stylesheet" href="/tshop-v132-conversion-storefront.css?v=1.32.0">',{html:true});
     head.append('<script defer src="/kch-thai-first.js?v=1.23.1"></script>',{html:true});
     head.append('<script defer src="/kch-footer-premium.js?v=1.27.0"></script>',{html:true});
     head.append('<script defer src="/kch-future-standard.js?v=1.28.1"></script>',{html:true});
     head.append('<script defer src="/kch-koonchaishop-commerce.js?v=1.30.1"></script>',{html:true});
     head.append('<script defer src="/kch-top-conversion.js?v=1.31.2"></script>',{html:true});
+    head.append('<script defer src="/kch-conversion-storefront.js?v=1.32.0"></script>',{html:true});
   }}).transform(response);
 };
 export async function onRequest({request,env,next}){
