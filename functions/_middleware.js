c6f3d270170920f3d6a0c7188318c6dacb5bb958
khonchaiherb-dev <khonchaiherb@gmail.com>
2026-09-02 15:13:48 +0700

    style: load readable futuristic storefront layer

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -23,6 +23,7 @@ const withCanonicalMetadata=(response,env)=>{
     head.append(`<meta property="og:url" content="${origin}/" />`,{html:true});
     head.append('<link rel="stylesheet" href="/tshop-v127-footer-premium.css?v=1.27.0">',{html:true});
     head.append('<link rel="stylesheet" href="/tshop-v128-future-standard.css?v=1.28.0">',{html:true});
+    head.append('<link rel="stylesheet" href="/tshop-v129-readable-future.css?v=1.29.0">',{html:true});
     head.append('<script defer src="/kch-thai-first.js?v=1.23.1"></script>',{html:true});
     head.append('<script defer src="/kch-footer-premium.js?v=1.27.0"></script>',{html:true});
     head.append('<script defer src="/kch-future-standard.js?v=1.28.1"></script>',{html:true});
