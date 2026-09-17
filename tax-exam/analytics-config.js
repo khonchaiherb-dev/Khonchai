16b095e31739f7f4462d743fecafd3c0887f6b67
khonchaiherb-dev <khonchaiherb@gmail.com>
2026-09-18 03:21:40 +0700

    Add K-EXAM analytics configuration scaffold

---
+++ tax-exam/analytics-config.js
@@ -0,0 +1,7 @@
+window.KEXAM_ANALYTICS_CONFIG=Object.freeze({
+  version:'13',
+  supabaseUrl:'',
+  supabaseAnonKey:'',
+  ingestFunction:'kexam-ingest',
+  dashboardFunction:'kexam-dashboard'
+});
